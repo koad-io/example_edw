d6bc31a9b051019adc475e3a6f08fc3dad06e86e
Karl <karl.moad@gmail.com>
2021-08-10 10:43:51 -0400

    add sample for CA_EPIC_ABSTRACT

---
+++ CAM/CA_EPIC_ABSTRACT_TEMP.sql
@@ -0,0 +1,7 @@
+INSERT INTO CA_EPIC_ABSTRACT
+SELECT *
+FROM A_TABLE
+WHERE A_FIELD = 'CONTITION VALUE'
+
+# This is a STANDIN/SAMPLE ELement stored in git to represent what will be a final 
+# source managed asset used in the Clinical Analytcis Warehouse processes
